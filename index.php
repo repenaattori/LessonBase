da5e0cf7693d128c6de18edb1bf6e027e4ddd107
repsukka <81526112+repsukka@users.noreply.github.com>
2022-11-01 20:35:54 +0200

    Harjoitus 1

--- index.php
+++ index.php
@@ -3,8 +3,17 @@ require('dbconnection.php');
 
 $dbcon = createSqliteConnection();
 
-$command = "INSERT INTO person (fname,lname) VALUES ('John','Doe')";
+$command = "INSERT INTO artists (Name) VALUES ('Tuomari Nurmio')";
 
 $dbcon->exec($command);
+
+$id = $dbcon->lastInsertId();
+
+$command = "INSERT INTO albums (Title, ArtistId) VALUES ('Punainen planeetta', $id)";
+
+$dbcon->exec($command);
+
+
+
 
 
